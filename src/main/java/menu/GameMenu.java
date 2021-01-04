2cb454ade32c581d3dba5d05f523224a3e335ce8
sicer <mansicer@qq.com>
2021-01-04 21:28:35 +0800

    display ip

--- src/main/java/menu/GameMenu.java
+++ src/main/java/menu/GameMenu.java
@@ -43,6 +43,10 @@ import util.ZipUtils;
 
 import java.io.*;
 import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Properties;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
@@ -72,7 +76,7 @@ public class GameMenu extends FXGLMenu {
         );
         Text IPContent = null;
         try {
-            IPContent = FXGL.getUIFactoryService().newText("@IP:" + Inet4Address.getLocalHost().getHostAddress());
+            IPContent = FXGL.getUIFactoryService().newText("@IP:" + getLocalHostLANAddress().getHostAddress());
         }
         catch (Exception e){
             e.printStackTrace();
@@ -395,5 +399,41 @@ public class GameMenu extends FXGLMenu {
                 null,null,new BackgroundPosition(null,0.1,true,null,0,true), null)));
         mdiWindow.setContentPane(pane);
         getContentRoot().getChildren().add(mdiWindow);
+    }
+    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            UnknownHostException unknownHostException = new UnknownHostException(
+                    "Failed to determine LAN address: " + e);
+            unknownHostException.initCause(e);
+            throw unknownHostException;
+        }
     }
 }
